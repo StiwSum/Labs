f9f6ed4765f79db2eb5e6a50c4e99a26bb59f9a1
еркыпе <@shytan-mashine>
2019-09-07 05:35:12 +0400

    Added entry point

---
+++ src/rpis71/badykov/wdad/Application.java
@@ -0,0 +1,7 @@
+package rpis71.badykov.wdad;
+
+public class Application {
+    public static void main(String[] args){
+        System.out.println("Ama Badykov Rafael and ama nat a makak");
+    }
+}
